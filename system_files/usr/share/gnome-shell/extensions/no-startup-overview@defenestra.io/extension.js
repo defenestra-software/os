34938eb3a64d41eee2f7387b88483a9a8a49c0a4
Taylor Ramirez <taylor.ramirez@defenestra.io>
2026-05-12 02:46:27 -0500

    Fixed extension not taking effect on first login

--- system_files/usr/share/gnome-shell/extensions/no-startup-overview@defenestra.io/extension.js
+++ system_files/usr/share/gnome-shell/extensions/no-startup-overview@defenestra.io/extension.js
@@ -3,11 +3,11 @@ import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 export default class NoStartupOverview {
     enable() {
+        const hide = () => Main.overview.hide();
         if (Main.layoutManager._startingUp) {
-            this._sid = Main.layoutManager.connect('startup-complete', () => {
-                Main.overview.hide();
-            });
+            this._sid = Main.layoutManager.connect('startup-complete', hide);
         }
+        hide();
     }
 
     disable() {
